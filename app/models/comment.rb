fd38ff7a0b2d7a9f77f25268c5e7e6db28e84196
erib <eri-b@users.noreply.github.com>
2019-06-24 15:07:10 -0400

    added gemfile, removed foreign key validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
   validates :body,  presence: true
-  validates :user_id,  presence: true
-  validates :link_id,  presence: true
   belongs_to :user
   belongs_to :link
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,5 @@
 class Link < ApplicationRecord
     validates :body,  presence: true
-    validates :user_id,  presence: true
     belongs_to :user
     has_many :comments
 end
